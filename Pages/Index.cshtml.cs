7acf4d1645bb13f4ed5aa1ed53fcdd463921eb80
unknown <kedar.alande@gmail.com>
2025-10-08 17:45:11 +0530

    added files

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    // Adding a public property to store the message
+    public string Message { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        // Setting the message to "Hello Safran"
+        Message = "Hello Safran";
+    }
+}
